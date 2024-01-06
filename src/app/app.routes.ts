9caba928a06fba8536d5dd8e75479a3cc635035e
div-github <divyachundawat865@gmail.com>
2024-01-06 14:18:04 +0530

    .git file path changed, sign-in component deleted, supposed to be inside App compo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { SignInComponent } from '../sign-in/sign-in.component';
 
-export const routes: Routes = [
-    {path: "",component: SignInComponent}
-];
+export const routes: Routes = [];
