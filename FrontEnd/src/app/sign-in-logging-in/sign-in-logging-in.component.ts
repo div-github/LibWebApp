f3857243de3ace2a9fcf544858090593e8b3ddc9
div-github <divyachundawat865@gmail.com>
2024-01-06 14:49:32 +0530

    Sign in and Sign up separate components

---
+++ FrontEnd/src/app/sign-in-logging-in/sign-in-logging-in.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-logging-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-in-logging-in.component.html',
+  styleUrl: './sign-in-logging-in.component.css'
+})
+export class SignInLoggingInComponent {
+
+}
