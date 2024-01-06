5e04143d04ef848b07e0e4a519fc86ea14eebaad
div-github <divyachundawat865@gmail.com>
2024-01-07 01:40:49 +0530

    BootStrap added and routing corrected

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -9,7 +9,6 @@ import { AdminAccountComponent } from './admin-account/admin-account.component';
 import { UserAccountComponent } from './user-account/user-account.component';
 import { IssueBookComponent } from './issue-book/issue-book.component';
 import { ReturnBookComponent } from './return-book/return-book.component';
-import { IssueBookChildScannerComponent } from './issue-book-child-scanner/issue-book-child-scanner.component';
 import { BookInventoryComponent } from './book-inventory/book-inventory.component';
 import { AppComponent } from './app.component';
 
@@ -18,28 +17,19 @@ export const routes: Routes = [
     path: '',
     component: AppComponent,
     children: [
-      //{ path: '', component: HeaderWithLogoComponent },
-
       {
         path: 'dashboard',
         component: DashboardComponent,
         children: [
           { path: '', component: BookInventoryComponent },
-          { path: 'signin', component: SignInLoggingInComponent },
-          { path: 'signup', component: SignUpRegistrationComponent },
-          {
-            path: 'issuebook',
-            component: IssueBookComponent,
-            children: [
-              { path: 'scanner', component: IssueBookChildScannerComponent },
-            ],
-          },
+          {path: 'issuebook', component: IssueBookComponent},
           { path: 'returnbook', component: ReturnBookComponent },
           { path: 'adminaccount', component: AdminAccountComponent },
           { path: 'useraccount', component: UserAccountComponent },
         ],
       },
-      { path: 'footer', component: FooterWithCopywriteAndContactComponent },
+      { path: 'signin', component: SignInLoggingInComponent },
+      { path: 'signup', component: SignUpRegistrationComponent },
     ],
   },
 ];
