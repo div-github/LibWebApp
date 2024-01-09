01933e8d70e20af1870a48bd614244aac70179de
div-github <divyachundawat865@gmail.com>
2024-01-10 00:25:06 +0530

    Reverted Some Changes

--- FrontEnd/src/app/sign-in-logging-in/sign-in-logging-in.component.ts
+++ FrontEnd/src/app/sign-in-logging-in/sign-in-logging-in.component.ts
@@ -1,15 +1,4 @@
-/*import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-sign-in-logging-in',
-  standalone: true,
-  imports: [],
-  templateUrl: './sign-in-logging-in.component.html',
-  styleUrl: './sign-in-logging-in.component.css'
-})
-export class SignInLoggingInComponent {
-
-}*/
 import { Component } from '@angular/core';
 
 @Component({
